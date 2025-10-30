e17482c601736b8d882b1b71d9c60a13437f6737
quietime11 <88940498+quietime11@users.noreply.github.com>
2025-10-30 09:42:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ from features.upload_data import load_data
 from features.group import select_grouping_mode
 from features.chart import plot_chart
 
-st.set_page_config(page_title="HVAC Thermal Graph Tool", page_icon="🌡️", layout="wide")
+st.set_page_config(page_title="Automatic HVAC Thermal Graph Tool", page_icon="🌡️", layout="wide")
 st.title("🌡️ HVAC Thermal Test Analyzer")
 
-uploaded_file = st.file_uploader("📤 Tải lên file CSV (VD: data_thermal.csv)", type=["csv"])
+uploaded_file = st.file_uploader("Upload file CSV (E.g.: data_thermal.csv)", type=["csv"])
 
 if uploaded_file is not None:
     df = load_data(uploaded_file)
@@ -14,4 +14,4 @@ if uploaded_file is not None:
         groups = select_grouping_mode(df)
         plot_chart(df, groups)
 else:
-    st.info("⬆️ Hãy tải lên file CSV để bắt đầu phân tích.")
+    st.info("Please upload file .CSV of the thermal data.")
